350e85a25ec0a164c3931c7c8619abaf63fd5df7
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-28 16:16:17 -0700

    add loading screen tips

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
+let randomTipIndex = Math.floor(Math.random() * 4);
+const randomTipArray = ["Take a sip of water!", "Do a minute of quick exercise!", "Take a snack break!",
+  "Do you have to go potty!? Go now!!!"]
+
 
 
 export default function Loading() {
+
+  randomTipIndex = Math.floor(Math.random() * 4);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         Cos<Text style={{ fontWeight: "normal", color: palette.green3 }}>Pack</Text>
       </Text>
+      <Text>{randomTipArray[randomTipIndex]}</Text>
       <ActivityIndicator size="large" color={palette.green2} />
     </View>
   )
