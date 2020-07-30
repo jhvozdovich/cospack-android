65c21c5d9c907aa8c4f7cddc8323c4605428ea50
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-30 11:48:50 -0700

    add color and images to update

--- src/components/CosplayDetails.js
+++ src/components/CosplayDetails.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert, TextInput } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Alert, TextInput, Image } from "react-native";
 import { AntDesign, Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 export default class CosplayDetails extends React.Component {
+  colorOptions = ["#008085", "#89D5D8", "#6C56BB", "#BB56B6", "#F1B90A", "#DD3B29", "#8B9794", "#232625"]
+
   state = {
     newCosplay: this.props.cosplayList.cosplay,
     newSeries: this.props.cosplayList.series,
+    newColor: this.props.cosplayList.color,
     newImage: this.props.cosplayList.image,
     newImagePrev: this.props.cosplayList.imagePrev,
     cosplayDetailsVisible: false,
@@ -29,9 +35,104 @@ export default class CosplayDetails extends React.Component {
     let cosplayList = this.props.cosplayList;
     cosplayList.cosplay = this.state.newCosplay;
     cosplayList.series = this.state.newSeries;
+    cosplayList.color = this.state.newColor;
     cosplayList.image = this.state.newImage;
     cosplayList.imagePrev = this.state.newImagePrev;
     this.props.updateCosplayDatabase(cosplayList)
+  }
+
+  renderColors() {
+    return this.colorOptions.map(color => (
+      <TouchableOpacity key={color} style={[styles.colorSelect, { backgroundColor: color }]}
+        onPress={() => this.setState({ newColor: color })} />
+    ))
+  }
+
+  _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [9, 16],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ newImage: result.uri });
+      }
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
+  _pickImagePrev = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ newImagePrev: result.uri });
+      }
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
+  renderImage = () => {
+    if (this.state.newImage) {
+      return (
+        <View style={styles.image}>
+          {this.state.newImage && <Image source={{ uri: this.state.newImage }} style={{
+            width: 90, height: 160, borderWidth: 2,
+            borderColor: "grey"
+          }} />}
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.image}>
+          <Image source={require("./../../public/ImageBlank.jpg")} style={{
+            width: 90, height: 160, borderWidth: 2,
+            borderColor: "grey"
+          }} />
+        </View>
+      )
+    }
+  }
+
+  renderImagePreview = () => {
+    if (this.state.newImagePrev) {
+      return (
+        <View style={styles.image}>
+          {this.state.newImagePrev && <Image source={{ uri: this.state.newImagePrev }} style={{
+            width: 160, height: 100, borderWidth: 2,
+            borderColor: "grey"
+          }} />}
+        </View>
+      )
+    } else if (this.state.newImage) {
+      return (
+        <View style={styles.image}>
+          {this.state.newImage && <Image source={{ uri: this.state.newImage }} style={{
+            width: 160, height: 100, borderWidth: 2,
+            borderColor: "grey"
+          }} />}
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.image}>
+          <Image source={require("./../../public/ImageBlank.jpg")} style={{
+            width: 160, height: 100, borderWidth: 2,
+            borderColor: "grey"
+          }} />
+        </View>
+      )
+    }
   }
 
   render() {
@@ -50,24 +151,39 @@ export default class CosplayDetails extends React.Component {
             <Text style={[styles.title]}>{this.props.cosplayList.cosplay}</Text>
           </View>
         </View>
-        <View style={{ flex: 12, marginHorizontal: 20 }}>
-          <View style={styles.inputBox}>
-            <Text style={styles.font}>Name:</Text>
-            <TextInput
-              style={[styles.input, { borderColor: this.props.cosplayList.color }]}
-              onChangeText={textCosplay => this.setState({ newCosplay: textCosplay })}
-              value={(this.state.newCosplay)} placeholder={this.props.cosplayList.cosplay} placeholderTextColor="black" />
-          </View>
-          <View style={styles.inputBox}>
-            <Text style={styles.font}>Series:</Text>
-            <TextInput
-              style={[styles.input, { borderColor: this.props.cosplayList.color }]}
-              onChangeText={textSeries => this.setState({ newSeries: textSeries })}
-              value={(this.state.newSeries)} placeholder={this.props.cosplayList.series} placeholderTextColor="black" />
+        <View style={{ flex: 16 }}>
+          <View style={{ marginHorizontal: 20 }}>
+            <View style={styles.inputBox}>
+              <Text style={styles.font}>Name:</Text>
+              <TextInput
+                style={[styles.input, { borderColor: this.props.cosplayList.color }]}
+                onChangeText={textCosplay => this.setState({ newCosplay: textCosplay })}
+                value={(this.state.newCosplay)} placeholder={this.props.cosplayList.cosplay} placeholderTextColor="black" />
+            </View>
+            <View style={styles.inputBox}>
+              <Text style={styles.font}>Series:</Text>
+              <TextInput
+                style={[styles.input, { borderColor: this.props.cosplayList.color }]}
+                onChangeText={textSeries => this.setState({ newSeries: textSeries })}
+                value={(this.state.newSeries)} placeholder={this.props.cosplayList.series} placeholderTextColor="black" />
+            </View>
           </View>
 
-          <Text> Color: {this.props.cosplayList.color}</Text>
-        </View>
+          <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 15, alignItems: "center" }}>
+            {this.renderColors()}
+          </View>
+
+          <TouchableOpacity onPress={this._pickImage}>
+            {this.renderImage()}
+          </TouchableOpacity>
+
+
+          <TouchableOpacity onPress={this._pickImagePrev}>
+            {this.renderImagePreview()}
+          </TouchableOpacity>
+
+
+        </View >
         <View style={{ flex: 2 }} >
           <TouchableOpacity style={{ alignSelf: "center" }}
             onPress={() => this.updateCosplay()} >
@@ -88,6 +204,13 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     alignItems: "center"
+  },
+  colorSelect: {
+    width: 35,
+    height: 35,
+    marginTop: 20,
+    marginHorizontal: 5,
+    borderRadius: 6
   },
   title: {
     color: "grey",
@@ -122,5 +245,10 @@ const styles = StyleSheet.create({
   inputBox: {
     flexDirection: "row",
     marginTop: 15
+  },
+  image: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5
   }
 });
